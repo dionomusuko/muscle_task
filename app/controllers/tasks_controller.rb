c7075f7cca9ab71f99a69f0c6145236bba6a02ee
dionomusuko <ruby.school@gmail.com>
2019-08-18 16:16:35 +0900

    add show edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,13 +17,21 @@ class TasksController < ApplicationController
     redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
   end
 
-  def update
+  def destroy
     @task = Task.find(params[:id])
-
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
+
+  def update
+    task = Task.find(params[:id])
+    task.update!(task_params)
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
+  end
+
+
 
   private
   def task_params
