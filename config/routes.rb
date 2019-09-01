02f7328d15e08d6353064f225f35a93f07f33d30
dionomusuko <dandybird1017@gmail.com>
2019-09-01 16:46:55 +0900

    add login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   namespace :admin do
     resources :users
   end
