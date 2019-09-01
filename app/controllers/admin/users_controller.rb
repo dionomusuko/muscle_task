27f6d2ad83f91f3b74cbea6b82248712a62b95f5
dionomusuko <dandybird1017@gmail.com>
2019-09-01 16:04:34 +0900

    admin comp

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,7 +4,7 @@ class Admin::UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(prams[:id])
+    @user = User.find(params[:id])
   end
 
   def new
@@ -12,7 +12,7 @@ class Admin::UsersController < ApplicationController
   end
 
   def edit
-    @user.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def create
