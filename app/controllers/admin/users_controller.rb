8f387af8f6189db78f959deb86f9a2ee8dbe821a
dionomusuko <dandybird1017@gmail.com>
2019-09-01 15:24:45 +0900

    add user controller index form edit

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,50 @@
+class Admin::UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(prams[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to admin_user_url(@user), notice: "ユーザー「#{@user.name}」を登録しました。"
+    else
+      render :new
+    end
+  end
+
+  def update
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to admin_user_url(@user), notice: "ユーザー「#{@user.name}」更新しました。"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = user.find(params[:id])
+    @user.destroy
+    redirect_to admin_user_url, notice: "ユーザー「#{@user.name}」を削除しました。"
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_18_155341) do
+ActiveRecord::Schema.define(version: 2019_08_18_160429) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2019_08_18_155341) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
