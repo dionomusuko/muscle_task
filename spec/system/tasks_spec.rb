20a08f6ba6f706425c2613029c18860653ab3f4d
dionomusuko <dandybird1017@gmail.com>
2019-09-05 00:19:58 +0900

    comp problem

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -13,13 +13,15 @@ describe 'タスク管理機能', type:  :system do
       click_button 'ログインする'
     end
 
+    shared_examples_for 'ユーザーAが作成したメニューが表示される' do
+      it { expect(page).to have_content '最初のメニュー' }
+    end
+
   describe '一覧表示機能'  do
     context 'ユーザーAがログインしているとき' do
       let(:login_user) { user_a }
 
-      it 'ユーザーAが作成したメニューが表示される' do
-        expect(page).to have_content '最初のメニュー'
-      end
+      it_behaves_like 'ユーザーAが作成したメニューが表示される'
     end
 
 
@@ -39,8 +41,29 @@ describe 'タスク管理機能', type:  :system do
           visit tasks_path(task_a)
         end
 
-        it 'ユーザーAが作成したメニューが表示される' do
-          expect(page).to have_content '最初のメニュー'
+       it_behaves_like 'ユーザーAが作成したメニューが表示される'
+      end
+    end
+
+    describe '新規作成機能' do
+      let(:login_user) { user_a }
+
+      before do
+        visit new_task_path
+        fill_in '名前', with: task_name
+        click_button '登録する'
+      end
+
+
+
+      context '新規作成画面で名前を入力しなかったとき' do
+        let(:task_name) { '' }
+
+        it 'エラー発生' do
+
+          within '#error_explanation' do
+            expect(page).to have_content '名前を入力してください'
+          end
         end
       end
     end
