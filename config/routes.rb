327eb38e28d494da57b92e03ce1f0c94d08460a7
dionomusuko <dandybird1017@gmail.com>
2019-11-08 00:33:04 +0900

    add posts-controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/edit'
+  get 'posts/new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
