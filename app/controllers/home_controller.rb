051608e746ec70df9978a25f5ff81615ce85c936
dionomusuko <ruby.school@gmail.com>
2019-11-07 12:00:24 +0900

    add home.index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   def index
+    @users = User.all
+    # @tasks = @users.task_params
+  end
 
+  private
+  def task_params
+    params.require(:task).permit(:name, :menu, :time, :rep, :set, :comment)
   end
 end
