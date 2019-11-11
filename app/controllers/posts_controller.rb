33cbbdc66184d68544cb115e5ed4414f4cf0bbdc
dionomusuko <ruby.school@gmail.com>
2019-11-11 18:12:36 +0900

    add pagenate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
+  PER = 10
+
   def index
-    @posts = Post.all.order(created_at: :desc)
+    @posts = Post.all.order(created_at: :desc).page(params[:page]).per(PER)
   end
 
   def show
